7ad44b1acb211bfed11748ed877353eb37dc1f2d
Astek27 <formov.efim@yandex.ru>
2026-04-20 22:11:18 +0300

    Add product_list view, template and main url

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Category, Product
+
+def product_list(request, category_slug=None):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    category = None
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'main/product/list.html',
+                  context={
+                      'category': category,
+                      'categories': categories,
+                      'products': products
+                  })
